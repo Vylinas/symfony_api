f53a49d84879d89c44dc1abb8840d9749b555316
Vylinas <sylvain.tera@gmail.com>
2019-03-07 11:52:24 +0100

    Uninstall Bundle
    FosRestBundle - JMSSerializer

--- config/bundles.php
+++ config/bundles.php
@@ -8,6 +8,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
-    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
